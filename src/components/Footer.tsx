628430d04e7ea89dbcd488a3b15959e5162347e6
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-06-20 13:55:58 +0300

    10

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import logo from '../assets/icon-facebook.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-black mt-5'>
+      <div className='container mx-auto px-4 py-4 '>
+      <div className='flex  justify-between text-white'>
+        <div>loopstudios</div>
+        <div className='flex'>
+          <img src={logo}/>
+          <img src={logo}/>
+          <img src={logo}/>
+          <img src={logo}/>
+        </div>
+      </div>
+      <div className='flex justify-between text-white mt-5'>
+        <div>
+          <ul className='flex gap-3'>
+            <li>about</li>
+            <li>carers</li>
+            <li>events</li>
+            <li>products</li>
+            <li>support</li>
+          </ul>
+        </div>
+        <div>2021. loopstudios All right reserved</div>
+      </div>
+    </div>
+    </div>
   )
 }
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import image from '../assets/desktop/image-interactive.jpg'
+import image1 from '../assets/desktop/image-deep-earth.jpg'
 
 const Main = () => {
   return (
-    <div className='container mx-auto px-4 py-4'>
+    <div className='container mx-auto px-4 py-4 mt-5'>
       <div className='  '>
         <div className='relative flex justify-between'>
           <img className='w-[800px]' src={image} />
@@ -14,8 +15,45 @@ const Main = () => {
         </div>
       </div>
 
-      <div>
-        
+      <div className='mt-14'>
+        <div className='flex justify-between'>
+          <div>OUR CONNECTION</div>
+          <div className='p-2 border-2'>SEE ALL</div>
+        </div>
+        <div className='grid grid-cols-4 gap-2'>
+          <div className='relative'>
+            <img src={image1}/>
+            <div className='absolute text-white bottom-4 left-4'>DEEP EARTH</div>
+          </div>
+          <div className='relative'>
+            <img src={image1}/>
+            <div className='absolute text-white bottom-4 left-4'>DEEP EARTH</div>
+          </div>
+          <div className='relative'>
+            <img src={image1}/>
+            <div className='absolute text-white bottom-4 left-4'>DEEP EARTH</div>
+          </div>
+          <div className='relative'>
+            <img src={image1}/>
+            <div className='absolute text-white bottom-4 left-4'>DEEP EARTH</div>
+          </div>
+          <div className='relative'>
+            <img src={image1}/>
+            <div className='absolute text-white bottom-4 left-4'>DEEP EARTH</div>
+          </div>
+          <div className='relative'>
+            <img src={image1}/>
+            <div className='absolute text-white bottom-4 left-4'>DEEP EARTH</div>
+          </div>
+          <div className='relative'>
+            <img src={image1}/>
+            <div className='absolute text-white bottom-4 left-4'>DEEP EARTH</div>
+          </div>
+          <div className='relative'>
+            <img src={image1}/>
+            <div className='absolute text-white bottom-4 left-4'>DEEP EARTH</div>
+          </div>
+        </div>
       </div>
     </div>
   )
